2dbcb3e02c150396d8e0b17d794efe0d43bf49e9
Nabil Chatbi <scinfu@gmail.com>
2016-12-20 20:46:50 +0100

    Linux test

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,13 @@
+//
+//  LinuxMain.swift
+//  SwiftSoup
+//
+//  Created by Nabil Chatbi on 20/12/16.
+//  Copyright © 2016 Nabil Chatbi. All rights reserved.
+//
+
+import XCTest
+import SwiftSoupTests
+
+var tests = [SwiftSoupTests]()
+XCTMain(tests)
