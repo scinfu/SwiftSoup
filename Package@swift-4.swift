71296f96b7f135398188945a311b1dc39695759e
Tim <0xtimc@gmail.com>
2017-09-27 14:05:43 +0100

    Add Swift 4 manifest file

---
+++ Package@swift-4.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftSoup",
+    products: [
+        .library(name: "SwiftSoup", targets: ["SwiftSoup"]),
+    ],
+    targets: [
+        .target(name: "SwiftSoup", path: "Sources"),
+        .testTarget(name: "SwiftSoupTests", dependencies: ["SwiftSoup"]),
+    ]
+)
