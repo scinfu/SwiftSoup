8096dc8ea2a448b2feccd22c33f319146aca64e1
Nabil Chatbi <n.chatbi@ntnext.it>
2017-10-04 21:54:50 +0200

    test

--- Example/SwiftSoup/ViewController.swift
+++ Example/SwiftSoup/ViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 import SwiftSoup
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
     var html : String!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let myURLString = "https://samdutton.wordpress.com/2015/04/02/high-performance-html/"
+        let myURLString = "https://facebook.com"
         guard let myURL = URL(string: myURLString) else {
             print("Error: \(myURLString) doesn't seem to be a valid URL")
             return
@@ -27,13 +28,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func parse(_ sender: Any) {
-        do {
-            for _ in 0...500 {
-                _ = try SwiftSoup.parse(html)
-            }
-        } catch {
-            print("Error")
+
+    @IBAction func doaction(_ sender: Any) {
+        for _ in 0...1000 {
+            _ = try! SwiftSoup.parse(html)
         }
-    }    
+    }
 }
+
