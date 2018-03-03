aca2c9c4d6dea60b4a24b2917fee6b3b531a9a92
Nabil Chatbi <scinfu@gmail.com>
2018-03-03 12:30:31 +0100

    Example

--- Example_/Pods/Target Support Files/Pods-SwiftSoup_Example/Pods-SwiftSoup_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftSoup_Example : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftSoup_Example
-@end
--- Example_/Pods/Target Support Files/SwiftSoup/SwiftSoup-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftSoup : NSObject
-@end
-@implementation PodsDummy_SwiftSoup
-@end
