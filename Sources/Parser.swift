bc15de109251367c6de38b347756f617d35fb447
Ambroise Decouttere <ambroise.decouttere@infomaniak.com>
2023-09-15 18:16:55 +0200

    Revert "fix: Async cleaner"
    This reverts commit f40c06d8d1227cf4147da43fd74dcf8e3ffb998b.

--- Sources/Parser.swift
+++ Sources/Parser.swift
@@ -198,24 +198,6 @@ public class Parser {
 		}
 		return doc
 	}
-    
-    @available(iOS 13.0.0, *)
-    public static func parseBodyFragment(_ bodyHtml: String, _ baseUri: String) async throws -> Document {
-        let doc: Document = Document.createShell(baseUri)
-        if let body: Element = doc.body() {
-            let nodeList: Array<Node> = try await parseFragment(bodyHtml, body, baseUri)
-            //var nodes: [Node] = nodeList.toArray(Node[nodeList.size()]) // the node list gets modified when re-parented
-            if nodeList.count > 0 {
-                for i in 1..<nodeList.count {
-                    try nodeList[i].remove()
-                }
-            }
-            for node: Node in nodeList {
-                try body.appendChild(node)
-            }
-        }
-        return doc
-    }
 
 	/**
 	 Utility method to unescape HTML entities from a string
