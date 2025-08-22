70ca523c1e6184dd40d35b57d851475ba5f17f8f
Filipp Kuznetsov <pro100filipp@icloud.com>
2025-08-22 23:52:30 +0300

    Add explicit support for tvOS 13+

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftSoup",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .watchOS(.v6)],
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
     products: [
         .library(name: "SwiftSoup", targets: ["SwiftSoup"])
     ],
