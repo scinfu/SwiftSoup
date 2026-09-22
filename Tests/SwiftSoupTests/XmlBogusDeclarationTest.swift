3ff3bb4ddb28c2289192e05d4a879ddcb216059f
Alex Ehlke <alex.ehlke@gmail.com>
2026-09-22 09:59:25 -0400

    Verify XML comment preservation and rebuilt declaration output

--- Tests/SwiftSoupTests/XmlBogusDeclarationTest.swift
+++ Tests/SwiftSoupTests/XmlBogusDeclarationTest.swift
@@ -3,21 +3,32 @@ import XCTest
 
 final class XmlBogusDeclarationTest: XCTestCase {
     func testEmptyProcessingInstructionRemainsAComment() throws {
-        for input in ["<??>", "<? ?>", "<! !>", "<?/?>"] {
+        for (input, data) in [("<??>", "??"), ("<? ?>", "? ?"), ("<! !>", " !"), ("<?/?>", "?/?")] {
             for tracked in [false, true] {
                 let doc = try Parser.xmlParser().settings(ParseSettings(true, true, tracked))
-                    .parseInput(input + "<root>tail</root>", "")
+                    .parseInput(input + "<root>tail</root>", "https://example.com/")
                 XCTAssertEqual(doc.getChildNodes().count, 2, input)
-                XCTAssertTrue(doc.childNode(0) is Comment, input)
+                let comment = try XCTUnwrap(doc.getChildNodes().first as? Comment, input)
+                XCTAssertEqual(comment.getData(), data, input)
+                XCTAssertEqual(comment.getBaseUri(), "https://example.com/", input)
+                doc.outputSettings().prettyPrint(pretty: false)
+                let rebuilt = String(decoding: try doc.outerHtmlUTF8WithoutSourceReuse(), as: UTF8.self)
+                XCTAssertEqual(rebuilt, "<!--" + data + "--><root>tail</root>", input)
                 XCTAssertEqual(try doc.select("root").text(), "tail", input)
             }
         }
     }
 
     func testValidDeclarationStillRetainsNameAndAttributes() throws {
-        let doc = try Parser.xmlParser().parseInput("<?xml version='1.0'?><root />", "")
-        let declaration = try XCTUnwrap(doc.childNode(0) as? XmlDeclaration)
-        XCTAssertEqual(declaration.name(), "xml")
-        XCTAssertEqual(try declaration.attr("version"), "1.0")
+        for tracked in [false, true] {
+            let doc = try Parser.xmlParser().settings(ParseSettings(true, true, tracked))
+                .parseInput("<?xml version='1.0'?><root />", "")
+            let declaration = try XCTUnwrap(doc.getChildNodes().first as? XmlDeclaration)
+            XCTAssertEqual(declaration.name(), "xml")
+            XCTAssertEqual(try declaration.attr("version"), "1.0")
+            doc.outputSettings().prettyPrint(pretty: false)
+            let rebuilt = String(decoding: try doc.outerHtmlUTF8WithoutSourceReuse(), as: UTF8.self)
+            XCTAssertEqual(rebuilt, "<?xml version=\"1.0\"?><root />")
+        }
     }
 }
